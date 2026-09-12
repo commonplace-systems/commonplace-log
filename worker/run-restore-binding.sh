f57924963e0fea2b4f7d7e1dcdd74dfc99e965ee
Jes Wolfe! <jes5199@gmail.com>
2026-09-12 19:26:41 +0000

    Select workers Vitest project in focused runner

--- worker/run-restore-binding.sh
+++ worker/run-restore-binding.sh
@@ -57,8 +57,9 @@ set +e
 cd "$worker_dir"
 timeout --signal=TERM --kill-after=5s 180s \
   env NODE_PATH="$provider_deps" "$provider_deps/.bin/vitest" run \
-  --config "$worker_dir/vitest.workers.config.ts" \
-  "$worker_dir/test/realm/restore.binding.workers.test.ts" \
+  --config "$worker_dir/vitest.config.ts" \
+  --project do \
+  "test/realm/restore.binding.workers.test.ts" \
   >"$output_dir/stdout.txt" 2>"$output_dir/stderr.txt"
 native_rc=$?
 set -e
